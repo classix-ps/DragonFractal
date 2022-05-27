4bbbd7f9726cb0267e306c51dc78c992b1e6abbc
Philip Suskin <philip.suskin@classix.de>
2022-05-28 00:10:26 +0200

    Use Gtk for keyboard input instead of readline()

--- dragonFractal.jl
+++ dragonFractal.jl
@@ -35,29 +35,41 @@ function calculateNextLines(frac::DragonFractal)
     end
 end
 
-c = GtkCanvas()
-win = GtkWindow(c, "Canvas")
-
-initialPoint = IntCoord(0, 1)
-fractal = DragonFractal([IntCoord(0, 0), initialPoint], [IntCoord(0, 0), rotate(initialPoint)])
-
-while true
+function drawFractal(frac::DragonFractal)
     @guarded draw(c) do widget
         ctx = getgc(c)
         h = height(c)
         w = width(c)
-
+    
         for i = 2:length(fractal.lines)
             rectangle(ctx, w / 2 + fractal.lines[i-1].x * 10, h / 2 + fractal.lines[i-1].y * 10, (fractal.lines[i] - fractal.lines[i-1]).x * 10 + 1, (fractal.lines[i] - fractal.lines[i-1]).y * 10 + 1)
             set_source_rgb(ctx, 0, 0, 0)
             fill(ctx)
         end
     end
-    show(c)
+end
 
-    if readline() == "q"
-        break
+c = GtkCanvas()
+win = GtkWindow(c, "Canvas")
+
+initialPoint = IntCoord(0, 1)
+fractal = DragonFractal([IntCoord(0, 0), initialPoint], [IntCoord(0, 0), rotate(initialPoint)])
+
+drawFractal(fractal)
+
+function keycall(w, event)
+    ch = Char(event.keyval)
+    if ch == 'q'
+        exit(86)
     else
         calculateNextLines(fractal)
+        drawFractal(fractal)
     end
 end
+signal_connect(keycall, win, "key-press-event")
+
+cond = Condition()
+endit(w) = notify(cond)
+signal_connect(endit, win, :destroy)
+showall(win)
+wait(cond)
