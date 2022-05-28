7afbc317dafba11c3bbca260f75b28a8d6945716
Philip Suskin <philip.suskin@classix.de>
2022-05-28 17:30:47 +0200

    First working (unoptimized) prototype

--- dragonFractal.jl
+++ dragonFractal.jl
@@ -35,16 +35,34 @@ function calculateNextLines(frac::DragonFractal)
     end
 end
 
-function drawFractal(frac::DragonFractal, off::Vector{T}) where T <: Real
+# TODO: Only draw what's in view
+# TODO: Zoom around center of canvas instead of origin
+function drawFractal(frac::DragonFractal, off::IntCoord, scale::Real)
     @guarded draw(c) do widget
         ctx = getgc(c)
         h = height(c)
         w = width(c)
     
+        set_source_rgb(ctx, 0, 0, 0)
         for i = 2:length(frac.lines)
-            rectangle(ctx, w / 2 + frac.lines[i-1].x * 10 + off[1], h / 2 + frac.lines[i-1].y * 10 + off[2], (frac.lines[i] - frac.lines[i-1]).x * 10 + 1, (frac.lines[i] - frac.lines[i-1]).y * 10 + 1)
-            set_source_rgb(ctx, 0, 0, 0)
-            fill(ctx)
+            move_to(ctx, w / 2 + frac.lines[i-1].x * 10 * scale + off.x, h / 2 + frac.lines[i-1].y * 10 * scale + off.y)
+            line_to(ctx, w / 2 + frac.lines[i].x * 10 * scale + off.x, h / 2 + frac.lines[i].y * 10 * scale + off.y)
+            stroke(ctx)
+        end
+    end
+end
+
+function drawNewLines(frac::DragonFractal, off::IntCoord, scale::Real)
+    @guarded draw(c) do widget
+        ctx = getgc(c)
+        h = height(c)
+        w = width(c)
+    
+        set_source_rgb(ctx, 0, 0, 0)
+        for i = (ceil(Int, length(frac.lines) / 2) + 1):length(frac.lines)
+            move_to(ctx, w / 2 + frac.lines[i-1].x * 10 * scale + off.x, h / 2 + frac.lines[i-1].y * 10 * scale + off.y)
+            line_to(ctx, w / 2 + frac.lines[i].x * 10 * scale + off.x, h / 2 + frac.lines[i].y * 10 * scale + off.y)
+            stroke(ctx)
         end
     end
 end
@@ -55,8 +73,8 @@ function resetCanvas()
         h = height(c)
         w = width(c)
     
-        rectangle(ctx, 0, 0, w, h)
         set_source_rgb(ctx, 1, 1, 1)
+        rectangle(ctx, 0, 0, w, h)
         fill(ctx)
     end
 end
@@ -67,40 +85,69 @@ win = GtkWindow(c, "Canvas")
 initialPoint = IntCoord(0, 1)
 fractal = DragonFractal([IntCoord(0, 0), initialPoint], [IntCoord(0, 0), rotate(initialPoint)])
 
-offset = [0, 0]
-zoom = 1
+offset = IntCoord(0, 0)
+oldMousePos = IntCoord(0, 0)
+zoom = 1.0
 
-drawFractal(fractal, offset)
+panning = false
 
-# https://rosettacode.org/wiki/Keyboard_input/Keypress_check#Julia
-function keycall(w, event)
-    #print(event)
+drawFractal(fractal, offset, zoom)
+
+function keypress(w, event)
     #print(event.keyval)
     if event.keyval == 65307 # esc
         exit(86)
-    elseif event.keyval >= 65361 && event.keyval <= 65364
-        if event.keyval == 65362 # up
-            offset[2] -= 10
-        end
-        if event.keyval == 65361 # left
-            offset[1] -= 10
-        end
-        if event.keyval == 65364 # down
-            offset[2] += 10
-        end
-        if event.keyval == 65363 # right
-            offset[1] += 10
-        end
-        resetCanvas()
     elseif event.keyval == 32 # space
         calculateNextLines(fractal)
+        drawNewLines(fractal, offset, zoom)
+    elseif event.keyval == 99 # c
+        global offset = IntCoord(0, 0)
+        resetCanvas()
+        drawFractal(fractal, offset, zoom)
     end
-    drawFractal(fractal, offset)
 end
-signal_connect(keycall, win, "key-press-event")
+signal_connect(keypress, win, "key-press-event")
+
+function mousepress(w, event)
+    if event.button == 1 # left-click
+        global oldMousePos = IntCoord(event.x, event.y)
+        global panning = true
+    end
+end
+signal_connect(mousepress, win, "button-press-event")
+
+function mouserelease(w, event)
+    if event.button == 1 # left-click
+        global panning = false
+    end
+end
+signal_connect(mouserelease, win, "button-release-event")
+
+function mousemove(w, event)
+    if panning
+        mousePos = IntCoord(event.x, event.y)
+        deltaPos = mousePos - oldMousePos
+        global offset += deltaPos
+        global oldMousePos = mousePos
+        resetCanvas()
+        drawFractal(fractal, offset, zoom)
+    end
+end
+signal_connect(mousemove, win, "motion-notify-event")
+
+function scroll(w, event)
+    if event.direction == 0 # up
+        global zoom += 0.1
+    elseif event.direction == 1 # down
+        global zoom -= 0.1
+    end
+    resetCanvas()
+    drawFractal(fractal, offset, zoom)
+end
+signal_connect(scroll, win, "scroll-event")
 
 cond = Condition()
 endit(w) = notify(cond)
 signal_connect(endit, win, :destroy)
-showall(win)
+show(c)
 wait(cond)
